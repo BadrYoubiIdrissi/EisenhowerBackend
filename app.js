66104bba7538f0ce6581e54a540256e6a431c2a9
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-18 01:48:53 +0200

    Serving static files

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ var app = express();
 
 var port  = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/build"));
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname+"/build/index.html");
+});
+
 app.listen(port);
