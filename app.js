f5e9321566d2fd1c22647af5480aada56f619196
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-19 04:32:49 +0200

    Syntax error

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if(process.env.NODE_ENV == "production"){
 
     app.use(express.static(path.join(__dirname, "/static")));
     app.get('*', function(req,res) {
-        res.sendFile(path.join(__dirname, "static/static/index.html"));
+        res.sendFile(path.join(__dirname, "static/index.html"));
     }); 
 
 }
