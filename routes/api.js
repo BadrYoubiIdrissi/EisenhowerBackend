f74cf4934158585a3b88a9b16b00d82357ef3135
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-08-02 15:21:43 +0200

    Added updating an array of tasks

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,20 @@ var Task = require("../models/task");
 var defaultTasks = require("../seeds/tasks/default");
 var bodyParser = require("body-parser");
 
+function upsertTask(task, res) {
+    if (task.id)
+        Task.update({ _id: task.id }, { $set: task.modification }, err => err ? res.status(400).json(err) : res.status(200).send("Task updated"));
+
+    else {
+        task = new Task(task);
+        task.save(err => err ? res.status(400).json(err) : res.status(201).json({ id: task._id }))
+    }
+}
+
+function updateTask(task) {
+    return Task.update({ _id: task.id }, { $set: task }, err => err ? console.error(err) : null);
+}
+
 var router = express.Router();
 
 router.use(bodyParser.json());
@@ -16,17 +30,16 @@ router.get("/tasks", function (req, res) {
 });
 
 router.post("/tasks", function (req, res) {
-    if (req.body.task.id)
-        Task.update({ _id: req.body.task.id }, { $set: req.body.task.modification }, err => err ? res.status(400).json(err) : res.status(200).send("Task updated"));
-
-    else {
-        task = new Task(req.body.task);
-        task.save(err => err ? res.status(400).json(err) : res.status(201).json({ id: task._id }))
+    if (req.body.constructor === Object)
+        upsertTask(req.body, res);
+    else if (req.body.constructor === Array){
+        req.body.map(task => updateTask(task))
+        res.send("Sync succeeded");
     }
 });
 
-router.delete("/tasks", function(req,res) {
-    Task.remove({_id:req.body.id}, err => err ? res.status(400).json(err) : res.status(200).send("Task deleted!"))
+router.delete("/tasks", function (req, res) {
+    Task.remove({ _id: req.query.id }, err => err ? res.status(400).json(err) : res.status(200).send("Task deleted!"));
 })
 
 
